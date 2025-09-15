bbc46cf34ccf680ad2068eca734dfc8289a6e235
soshihojo <soshihojo@ad-hood.com>
2025-09-15 20:16:09 +0900

    feat: Adjust icon color in Supported Genres section

--- src/components/SupportedGenres.jsx
+++ src/components/SupportedGenres.jsx
@@ -24,7 +24,7 @@ const SupportedGenres = () => {
       <div className="text-center">
         {genres.map((genre, index) => (
           <span key={index} className="badge rounded-pill bg-secondary m-1 p-2 fs-6 fw-normal d-inline-flex align-items-center">
-            <span className="material-icons me-1" style={{ verticalAlign: 'middle', fontSize: '1em' }}>{genre.icon}</span>
+            <span className="material-icons me-1 text-info" style={{ verticalAlign: 'middle', fontSize: '1em' }}>{genre.icon}</span>
             {genre.name}
           </span>
         ))}
