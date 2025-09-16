733cafc2f5f1465077d8d585583eb9d99eb16555
soshihojo <soshihojo@ad-hood.com>
2025-09-16 18:13:39 +0900

    feat: Change icon color in Description section to primary blue

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -9,11 +9,13 @@ const Description = () => {
           <p className="lead">アドベンチマークは商材ジャンルや広告媒体ごとの、配信データが確認できるツールです。</p>
           <h5 className="fw-bold mt-4">アドベンチマークで見れる指標</h5>
           <ul className="list-unstyled">
-            <li><span className="material-icons me-2" style={{ verticalAlign: 'middle' }}>check_circle</span>CTR</li>
-            <li><span className="material-icons me-2" style={{ verticalAlign: 'middle' }}>check_circle</span>CVR</li>
-            <li><span className="material-icons me-2" style={{ verticalAlign: 'middle' }}>check_circle</span>CPC</li>
-            <li><span className="material-icons me-2" style={{ verticalAlign: 'middle' }}>check_circle</span>CPA</li>
-            <li><span className="material-icons me-2" style={{ verticalAlign: 'middle' }}>check_circle</span>COST</li>
+            <li><span className="material-icons me-2 text-primary" style={{ verticalAlign: 'middle' }}>check_circle</span>CTR</li>
+            <li><span className="material-icons me-2 text-primary" style={{ verticalAlign: 'middle' }}>check_circle</span>CVR</li>
+            <li><span className="material-icons me-2 text-primary" style={{ verticalAlign: 'middle' }}>check_circle</span>CPC</li>
+            <li><span className="material-icons me-2 text-primary" style={{ verticalAlign: 'middle' }}>check_circle</span>CVR</li>
+            <li><span className="material-icons me-2 text-primary" style={{ verticalAlign: 'middle' }}>check_circle</span>CPC</li>
+            <li><span className="material-icons me-2 text-primary" style={{ verticalAlign: 'middle' }}>check_circle</span>CPA</li>
+            <li><span className="material-icons me-2 text-primary" style={{ verticalAlign: 'middle' }}>check_circle</span>COST</li>
           </ul>
           <p className="mt-4">上記指標のそれぞれ「平均値・中央値・下位25％・上位25％」をみることができます。</p>
           <p>データは様々な商材や予算規模などで、偏りが出過ぎないように精査しています。</p>
